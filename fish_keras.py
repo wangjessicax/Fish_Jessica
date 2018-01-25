334947309f233012d9b8b1372ee7e1a4c9c76c72
wangjessicax <wang.jessicax@gmail.com>
2018-01-25 16:51:00 -0500

    hey

--- fish_keras.py
+++ fish_keras.py
@@ -91,5 +91,5 @@ if __name__ == "__main__":
                 break
         if len(agent.memory) > batch_size:
             agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
+        if e % 10 == 0:
+            agent.save("./save/cartpole-dqn.h5")
